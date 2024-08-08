faba0ecf39a0cf60f7e2958c29c94de23597bf1f
Kunal-Rathour <knl.rathour@gmail.com>
2024-08-08 19:37:22 +0530

    written the script for basic functionality in the caluclator

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const inputContainer = document.querySelector("#numberinput")
 const numberButtons = document.querySelectorAll(".numberdiv");
 const backspace = document.querySelector("#backspace");
 const clearButtons = document.querySelectorAll(".cleardiv");
-let bufferValue;
-let containerValue = inputContainer.innerText;
+let bufferText;
+const bufferContainer = document.querySelector(".buffer");
+const calculationObject = {
+    firstNum: "",
+    sign: "",
+    secondNum: "",
+}
+bufferText = `${calculationObject['firstNum']} ${calculationObject['sign']} ${calculationObject["secondNum"]}`
+bufferContainer.innerText = bufferText;
+let containerValue = parseInt(inputContainer.innerText);
 numberButtons.forEach(button => {
     button.addEventListener("click", () => {
         let inputText = parseInt(button.textContent);
@@ -19,48 +27,90 @@ backspace.addEventListener("click", () => {
 })
 const mathematicButton = document.querySelectorAll(".mathematicdiv")
 mathematicButton.forEach(button => {
-    let id = button.id;
-    let firstInput = inputContainer.innerText;
-    button.addEventListener("click", () => arithematic(firstInput, firstInput, id))
+    button.addEventListener("click", () => {
+        calculationObject["firstNum"] = parseInt(inputContainer.innerText);
+        calculationObject["sign"] = button.id;
+        inputContainer.innerText = "";
+        bufferText = `${calculationObject['firstNum']} ${calculationObject['sign']} ${calculationObject["secondNum"]}`
+        bufferContainer.innerText = bufferText;
+    })
+})
+const equalTo = document.querySelector("#equalto");
+equalTo.addEventListener("click", () => {
+    calculationObject["secondNum"] = parseInt(inputContainer.innerText);
+    bufferText = `${calculationObject['firstNum']} ${calculationObject['sign']} ${calculationObject["secondNum"]}`
+    bufferContainer.innerText = bufferText;
+    bufferContainer.innerText = bufferText;
+    arithematic(calculationObject["firstNum"], calculationObject["sign"], calculationObject["secondNum"])
+})
+clearButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (button.id == "clearCurrent") {
+            inputContainer.innerText = "";
+            bufferText = `${calculationObject['firstNum']} ${calculationObject['sign']} ${calculationObject["secondNum"]}`
+            bufferContainer.innerText = bufferText;
+        }
+        else {
+            calculationObject["firstNum"] = "";
+            calculationObject["sign"] = "";
+            calculationObject["secondNum"] = "";
+            inputContainer.innerText = "";
+            bufferText = `${calculationObject['firstNum']} ${calculationObject['sign']} ${calculationObject["secondNum"]}`
+            bufferContainer.innerText = bufferText;
+        }
+    })
 })
 clearButtons.forEach(button => {
     button.addEventListener("click", () => {
         console.log(button.innerText)
+
     })
 })
-function arithematic(firstnumber, secondNumber, sign) {
-    console.log(firstnumber)
-    console.log(secondNumber)
+function arithematic(firstInput, sign, secondInput) {
     switch (sign) {
         case "divide":
-            console.log("Divide");
+            inputContainer.innerText = firstInput / secondInput;
+            bufferText = `${calculationObject['firstNum']} ${calculationObject['sign']} ${calculationObject["secondNum"]}`
+            bufferContainer.innerText = bufferText;
             break;
         case "plus":
-            console.log("Add");
+            inputContainer.innerText = firstInput + secondInput;
+            bufferText = `${calculationObject['firstNum']} ${calculationObject['sign']} ${calculationObject["secondNum"]}`
+            bufferContainer.innerText = bufferText;
             break;
         case "minus":
-            console.log("Subtract");
+            inputContainer.innerText = firstInput - secondInput;
+            bufferText = `${calculationObject['firstNum']} ${calculationObject['sign']} ${calculationObject["secondNum"]}`
+            bufferContainer.innerText = bufferText;
             break;
         case "multiply":
-            console.log("multiply");
+            inputContainer.innerText = firstInput * secondInput;
+            bufferText = `${calculationObject['firstNum']} ${calculationObject['sign']} ${calculationObject["secondNum"]}`
+            bufferContainer.innerText = bufferText;
             break;
         case "percentage":
-            console.log("Percentage");
+            inputContainer.innerText = firstInput * (secondInput / 100)
+            bufferText = `${calculationObject['firstNum']} ${calculationObject['sign']} ${calculationObject["secondNum"]}`
+            bufferContainer.innerText = bufferText;
             break;
         case "ratio":
-            console.log("Ratio");
+            inputContainer.innerText = 1 / firstInput;
+            bufferText = `${calculationObject['firstNum']} ${calculationObject['sign']} ${calculationObject["secondNum"]}`
+            bufferContainer.innerText = bufferText;
             break;
         case "square":
-            console.log("Square");
+            inputContainer.innerText = firstInput * firstInput;
+            bufferText = `${calculationObject['firstNum']} ${calculationObject['sign']} ${calculationObject["secondNum"]}`
+            bufferContainer.innerText = bufferText;
             break;
         case "root":
-            console.log("Root")
+            inputContainer.innerText = Math.sqrt(firstInput);
+            bufferText = `${calculationObject['firstNum']} ${calculationObject['sign']} ${calculationObject["secondNum"]}`
+            bufferContainer.innerText = bufferText;
             break;
         case "signchange":
-            console.log("SignChange");
+            inputContainer.innerText = firstInput * -1;
             break;
-        case "decimal":
-            console.log("Decimal");
-            break;
+
     }
 }
